1cae59172728c923f4bdc101a8dab774fb743232
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-10-16 17:47:14 +0700

    Update main.dart

--- 05_Antarmuka_Pengguna_Lanjutan/GUIDED/main.dart
+++ 05_Antarmuka_Pengguna_Lanjutan/GUIDED/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:modul_5/flexibel_extended.dart';
+//import 'package:modul_5/Costom.dart';
+//import 'package:modul_5/jenis_list_viwe.dart';
 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FlexibleExpandedScreen(),
+      //JenisListView(), 
+      //Custom(), 
+      
+   );
+  }
+}
