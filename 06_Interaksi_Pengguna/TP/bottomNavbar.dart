aee1d1e706630e9ba9cb46df00a190a4679d7ca2
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-10-22 23:10:17 +0700

    Update bottomNavbar.dart

--- 06_Interaksi_Pengguna/TP/bottomNavbar.dart
+++ 06_Interaksi_Pengguna/TP/bottomNavbar.dart
@@ -1,1 +1,114 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBarExample extends StatefulWidget {
+  @override
+  _BottomNavBarExampleState createState() => _BottomNavBarExampleState();
+}
+
+class _BottomNavBarExampleState extends State<BottomNavBarExample> {
+  int _selectedIndex = 0;
+
+List<Widget> _widgetOptions = <Widget>[
+  HomeScreen(),
+  WisataScreen(),
+  ProfilScreen(),
+];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Awesome Bottom Navigation'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: AnimatedSwitcher(
+        duration: Duration(milliseconds: 300),
+        child: _widgetOptions[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Wisata',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purpleAccent,
+        unselectedItemColor: Colors.white70,
+        backgroundColor: Colors.black87,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        showUnselectedLabels: false,
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[50],
+      child: Center(
+        child: Text(
+          'Ini adalah halaman Home',
+          style: TextStyle(fontSize: 24, color: Colors.blueAccent, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class WisataScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange[50],
+      child: Center(
+        child: Text(
+          'Ini adalah halaman Wisata',
+          style: TextStyle(fontSize: 24, color: Colors.orange, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green[50],
+      child: Center(
+        child: Text(
+          'Ini adalah halaman Profil',
+          style: TextStyle(fontSize: 24, color: Colors.green, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
 
