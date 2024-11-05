2e5eff0ed0bb19b121ea7007c0c9dc9d85dca7d3
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-11-05 22:37:58 +0700

    Update main.dart

--- 07_Navigasi & Notifikasi/GUIDED/main.dart
+++ 07_Navigasi & Notifikasi/GUIDED/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:modul_7/pages/mypage.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Mypage(),
+    );
+  }
+}
