19b5522f21157daddab5d13fa74723b25ab02617
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-11-05 23:31:24 +0700

    Update main.dart

--- 07_Navigasi & Notifikasi/UNGUIDED/main.dart
+++ 07_Navigasi & Notifikasi/UNGUIDED/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:modul_77/pages/home_page.dart';
 
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-commerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: HomePage(),
+    );
+  }
+}
