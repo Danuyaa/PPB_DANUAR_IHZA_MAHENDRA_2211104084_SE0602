f6ec44085b46f59f67f7c79fe664eafbf34c0b54
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-11-19 23:22:09 +0700

    Update main.dart

--- 09_API_Perangkat_Keras/GUIDED/main.dart
+++ 09_API_Perangkat_Keras/GUIDED/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum/camera_screen.dart';
+import 'package:praktikum/image_picker.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:
+
+          // MyCameraScreen()
+
+          ImagePickerScreen(
+        ImageSourceType.gallery,
+      ),
+    );
+  }
+}
