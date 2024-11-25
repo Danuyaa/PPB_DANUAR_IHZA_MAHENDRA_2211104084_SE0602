3a8160a6ae14f44e07289db8b8ff78afd3a2588e
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-11-25 22:49:51 +0700

    Update main.dart

--- 10_Data_Storage/GUIDED/main.dart
+++ 10_Data_Storage/GUIDED/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:modul_10/view/my_db_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Database Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyDatabaseView(), // Mengganti halaman utama ke MyDatabase
+    );
+  }
+}
 
