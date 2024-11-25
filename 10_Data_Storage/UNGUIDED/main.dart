cd6e14fa44d82e6da72fd0ac15825b31e2ad5216
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-11-25 22:47:43 +0700

    Update main.dart

--- 10_Data_Storage/UNGUIDED/main.dart
+++ 10_Data_Storage/UNGUIDED/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:modul_10/pages/pages.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplikasi Biodata Mahasiswa',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: HomePage(),
+    );
+  }
+}
 
