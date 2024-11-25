65e6803b88222fe162c36da55ac859ce6590e7e7
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2024-11-25 23:07:24 +0700

    Update main.dart

--- 09_API_Perangkat_Keras/UNGUIDED/main.dart
+++ 09_API_Perangkat_Keras/UNGUIDED/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Actions',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
 
